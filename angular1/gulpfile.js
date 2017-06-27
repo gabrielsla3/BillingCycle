6c0c90b06bcb250b024f5a4946cc6b4b9a582d5c
Gabriel Hahn Schaeffer <gabriel_hahn@hotmail.com>
2017-06-26 22:40:11 -0300

    Front end
    Commit inicial do front end

---
+++ angular1/gulpfile.js
@@ -0,0 +1,20 @@
+
+const gulp = require('gulp');
+const klaw = require('klaw')
+const util = require('gulp-util');
+
+require('./gulpTasks/app');
+require('./gulpTasks/deps');
+require('./gulpTasks/server');
+
+//Task default que é chamada sempre que executar o comando do gulp no console
+gulp.task('default', function(){    
+    //Se na hora de executar o comando, tiver o --production conforme definido no json
+    if(util.env.production){
+        gulp.start('deps', 'app');
+    }
+    //Caso tiver em desenvolvimento, sobe o "server"" também para monitorar os arquivos
+    else{
+        gulp.start('deps', 'app', 'server');
+    }
+});
---
+++ app/index.js
@@ -0,0 +1,1 @@
+angular.module('billingCycleApp', ['ui.router', 'ngAnimate', 'toastr'])
